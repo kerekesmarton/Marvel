b0b9c787eb9847fcc31b6bc0dcce510c95b1d9f8
Kerekes Marton <kerekes.j.marton@gmail.com>
2020-01-04 10:46:03 +0100

    documentation on parsers

--- Core/Data/DataParsing/DataParsing.swift
+++ Core/Data/DataParsing/DataParsing.swift
@@ -7,14 +7,26 @@ import Foundation
 import Domain
 import RealmSwift
 
+/// Description contained in data layer to abstract data transorfmations
 protocol Model: Equatable {
     associatedtype Entity
+    /// creates an instance of a the matching domain object
     func generateEntity() throws -> Entity
+    /// initialise a data model from a domain object
+    /// - Parameter entity: The domain object
     init(from entity: Entity) throws
+    /// Used for matching persisted items
+    /// - Parameter parameters: pass items to match against, usually a URI, Id, etc. dependends on implementation and API.
     func matches(parameters: [String: String]) -> Bool
 }
 
 protocol DataParsing {
+    /**
+     Parser data into a given domain object
+     - Parameters:
+     - data: data to be transformed
+     - source: where the data is coming from
+     */
     func decode<T>(from data: Data, source: URL?) throws -> T?
 }
 
@@ -74,7 +86,16 @@ struct ErrorDataParser: DataParsing {
 }
 
 protocol DataEncoding {
+    /**
+     Parser a domain object into data
+     - Parameter entity: The domain object to be transformed to data
+     */
     func encode<U>(from entity: U) throws -> Data?
+    
+    /**
+     Creates a data model from a domain object
+     - Parameter entity:  The domain object
+     */
     func model<U, T: Model>(from entity: U) throws -> T
 }
 
@@ -89,7 +110,7 @@ extension DataEncoding {
 
 
 public struct GenericDataEncoder: DataEncoding {
-
+    
     func model<U, T>(from entity: U) throws -> T where T : Model {
         switch entity.self {
         default:
